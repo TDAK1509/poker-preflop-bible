2334d5b1ba2dd9ceef62687899b86b72787168a7
Khuong Tran <dbcgdhh@gmail.com>
2021-06-13 15:43:21 +0700

    Init tabs to show position content

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,79 @@
 <script lang="ts">
-  export let name: string;
+  enum Tab {
+    UG,
+    MP,
+    CO,
+    BT,
+    SB,
+    BB,
+  }
+
+  let activeTab: Tab = Tab.UG;
+
+  function changeTab(tab: Tab) {
+    activeTab = tab;
+  }
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  <ul>
+    <li>
+      <button
+        on:click={() => {
+          changeTab(Tab.UG);
+        }}>UG</button
+      >
+    </li>
+    <li>
+      <button
+        on:click={() => {
+          changeTab(Tab.MP);
+        }}>MP</button
+      >
+    </li>
+    <li>
+      <button
+        on:click={() => {
+          changeTab(Tab.CO);
+        }}>CO</button
+      >
+    </li>
+    <li>
+      <button
+        on:click={() => {
+          changeTab(Tab.BT);
+        }}>BT</button
+      >
+    </li>
+    <li>
+      <button
+        on:click={() => {
+          changeTab(Tab.SB);
+        }}>SB</button
+      >
+    </li>
+    <li>
+      <button
+        on:click={() => {
+          changeTab(Tab.BB);
+        }}>BB</button
+      >
+    </li>
+  </ul>
+  {#if activeTab === Tab.UG}
+    <p>UTG</p>
+  {:else if activeTab === Tab.MP}
+    <p>MP</p>
+  {:else if activeTab === Tab.CO}
+    <p>CO</p>
+  {:else if activeTab === Tab.BT}
+    <p>BTN</p>
+  {:else if activeTab === Tab.SB}
+    <p>SB</p>
+  {:else}
+    <p>BB</p>
+  {/if}
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
