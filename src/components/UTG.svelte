c11ddb98871b16b7b575e7304bbac5cf33b19e13
Khuong Tran <dbcgdhh@gmail.com>
2021-06-13 20:15:17 +0700

    Use each to show images

--- src/components/UTG.svelte
+++ src/components/UTG.svelte
@@ -1,11 +1,23 @@
-<script>
+<script lang="ts">
   import RangeImage from "./RangeImage.svelte";
+
+  interface RangeImageInterface {
+    title: string;
+    imageSrc: string;
+    alt: string;
+  }
+
+  let images: RangeImageInterface[] = [
+    {
+      title: "Open",
+      imageSrc: "assets/images/open_UTG.png",
+      alt: "Open UTG Image",
+    },
+  ];
 </script>
 
 <h3>Under the gun (UTG)</h3>
 
-<RangeImage
-  title="Open"
-  imageSrc="assets/images/open_UTG.png"
-  alt="Open UTG Image"
-/>
+{#each images as image}
+  <RangeImage {...image} />
+{/each}
