98e534a0dd29da8aa9379cb275032df0cc305f08
Khuong Tran <dbcgdhh@gmail.com>
2021-06-14 19:21:11 +0700

    Style range image

--- src/components/RangeImage.svelte
+++ src/components/RangeImage.svelte
@@ -4,7 +4,21 @@
   export let alt: string = "";
 </script>
 
-<figure>
+<figure class="range-image">
   <figcaption>{title}</figcaption>
   <img src={imageSrc} {alt} />
 </figure>
+
+<style>
+  .range-image {
+    margin-top: 2em;
+    padding-top: 1em;
+    border-top: 1px solid #ccc;
+  }
+
+  .range-image figcaption {
+    font-weight: 700;
+    text-align: left;
+    margin-bottom: 0.5em;
+  }
+</style>
