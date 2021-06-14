9486cef1148a0d6a419586867eb6e2be359c57c0
Khuong Tran <dbcgdhh@gmail.com>
2021-06-14 07:27:34 +0700

    Add images CO

--- src/components/CO.svelte
+++ src/components/CO.svelte
@@ -1,5 +1,48 @@
-<script>
-  // your script goes here
+<script lang="ts">
+  import RangeImage from "./RangeImage.svelte";
+  import type { RangeImageInterface } from "../types/RangeImage";
+
+  let images: RangeImageInterface[] = [
+    {
+      title: "Open",
+      imageSrc: "assets/images/open_CO.png",
+      alt: "open_CO.png",
+    },
+    {
+      title: "3bet CO v UTG",
+      imageSrc: "assets/images/3bet_COvUTG.png",
+      alt: "3bet_COvUTG.png",
+    },
+    {
+      title: "3bet CO v MP",
+      imageSrc: "assets/images/3bet_COvMP.png",
+      alt: "3bet_COvMP.png",
+    },
+    {
+      title: "vs 3bet IP",
+      imageSrc: "assets/images/vs3bet_CO-IP.png",
+      alt: "vs3bet_CO-IP.png",
+    },
+    {
+      title: "vs 3bet OOP",
+      imageSrc: "assets/images/vs3bet_CO-OOP.png",
+      alt: "vs3bet_CO-OOP.png",
+    },
+    {
+      title: "vs 4bet CO v UTG",
+      imageSrc: "assets/images/vs4bet_COvUTG.png",
+      alt: "vs4bet_COvUTG.png",
+    },
+    {
+      title: "vs 4bet CO v MP",
+      imageSrc: "assets/images/vs4bet_COvMP.png",
+      alt: "vs4bet_COvMP.png",
+    },
+  ];
 </script>
 
-<p>CO</p>
+<h3>Cut off (CO)</h3>
+
+{#each images as image}
+  <RangeImage {...image} />
+{/each}
