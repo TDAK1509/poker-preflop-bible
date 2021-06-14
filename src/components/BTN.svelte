01d3576ea39081618604050fc09014030a9df825
Khuong Tran <dbcgdhh@gmail.com>
2021-06-14 07:30:42 +0700

    Add images BTN

--- src/components/BTN.svelte
+++ src/components/BTN.svelte
@@ -1,5 +1,48 @@
-<script>
-  // your script goes here
+<script lang="ts">
+  import RangeImage from "./RangeImage.svelte";
+  import type { RangeImageInterface } from "../types/RangeImage";
+
+  let images: RangeImageInterface[] = [
+    {
+      title: "Open",
+      imageSrc: "assets/images/open_BTN.png",
+      alt: "open_BTN.png",
+    },
+    {
+      title: "3bet v UTG",
+      imageSrc: "assets/images/3bet_BTNvUTG.png",
+      alt: "3bet_BTNvUTG.png",
+    },
+    {
+      title: "3bet v MP",
+      imageSrc: "assets/images/3bet_BTNvMP.png",
+      alt: "3bet_BTNvMP.png",
+    },
+    {
+      title: "3bet v CO",
+      imageSrc: "assets/images/3bet_BTNvCO.png",
+      alt: "3bet_BTNvCO.png",
+    },
+    {
+      title: "vs 3bet",
+      imageSrc: "assets/images/vs3bet_BTN.png",
+      alt: "vs3bet_BTN.png",
+    },
+    {
+      title: "vs 4bet v UTG",
+      imageSrc: "assets/images/vs4bet_BTNvUTG.png",
+      alt: "vs4bet_BTNvUTG.png",
+    },
+    {
+      title: "vs 4bet v MP",
+      imageSrc: "assets/images/vs4bet_BTNvMP.png",
+      alt: "vs4bet_BTNvMP.png",
+    },
+  ];
 </script>
 
-<p>BTN</p>
+<h3>Button (BTN)</h3>
+
+{#each images as image}
+  <RangeImage {...image} />
+{/each}
