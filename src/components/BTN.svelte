9c3f6f3d75d27986c34c660dbb5b4d30f70e85e8
Khuong Tran <dbcgdhh@gmail.com>
2021-06-16 10:35:22 +0700

    Apply filter to BTN

--- src/components/BTN.svelte
+++ src/components/BTN.svelte
@@ -7,11 +7,13 @@
       title: "Open",
       imageSrc: "assets/images/open_BTN.png",
       alt: "open_BTN.png",
+      className: "open",
     },
     {
       title: "3bet v UTG",
       imageSrc: "assets/images/3bet_BTNvUTG.png",
       alt: "3bet_BTNvUTG.png",
+      className: "three-bet",
     },
     {
       title: "3bet v MP",
@@ -27,11 +29,13 @@
       title: "vs 3bet",
       imageSrc: "assets/images/vs3bet_BTN.png",
       alt: "vs3bet_BTN.png",
+      className: "vs3bet",
     },
     {
       title: "vs 4bet v UTG",
       imageSrc: "assets/images/vs4bet_BTNvUTG.png",
       alt: "vs4bet_BTNvUTG.png",
+      className: "vs4bet",
     },
     {
       title: "vs 4bet v MP",
